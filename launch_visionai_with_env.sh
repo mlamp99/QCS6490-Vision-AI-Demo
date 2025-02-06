ecd0588c9ec83fbe452e030b5f028cef03e4cb10
ljkeller <kellerjlucas@gmail.com>
2025-02-06 10:43:34 -0700

    Remove cam signal, add support for icon launch
    Launch from weston.ini with new icon. AWK script to come.

--- launch_visionai_with_env.sh
+++ launch_visionai_with_env.sh
@@ -3,11 +3,10 @@
 mount -o rw,remount /
 setenforce 0
 
+# Default to relative path, but allow override via environment variable. Helpful for dev vs prod env
+VISIONAI_PATH="${VISIONAI_PATH_OVERRIDE:-./visionai.py}"
+
 export XDG_RUNTIME_DIR=/dev/socket/weston
 export WAYLAND_DISPLAY=wayland-1
 
-#TODO: Delete when camera auto selection is implemented
-export CAM1=/dev/video2
-export CAM2=/dev/video4
-
-./visionai.py
+exec "$VISIONAI_PATH"
