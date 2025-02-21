36ab712f61e4312c472240252626fea7131590ac
ljkeller <kellerjlucas@gmail.com>
2025-02-21 12:44:36 -0700

    Glade UI/Graphing improvements
    Expand the graphing area, move the legend over, and get rid of the
    arbitrary y offset for the streams.

--- vai/handler.py
+++ vai/handler.py
@@ -244,13 +244,13 @@ class Handler:
             # TODO: Pull up allocation/sizing to previous function closer to init
             allocation = self.DrawArea1.get_allocation()
             self.DrawArea1_x = allocation.x
-            self.DrawArea1_y = allocation.y + HEIGHT_OFFSET
+            self.DrawArea1_y = allocation.y
             self.DrawArea1_w = MAX_WINDOW_WIDTH
             self.DrawArea1_h = MAX_WINDOW_HEIGHT
 
             allocation = self.DrawArea2.get_allocation()
             self.DrawArea2_x = allocation.x
-            self.DrawArea2_y = allocation.y + HEIGHT_OFFSET
+            self.DrawArea2_y = allocation.y
             self.DrawArea2_w = MAX_WINDOW_WIDTH
             self.DrawArea2_h = MAX_WINDOW_HEIGHT
 
