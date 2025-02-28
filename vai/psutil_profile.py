3c985ddafd911c228a94e8f85b0318e0ebe522fd
ljkeller <kellerjlucas@gmail.com>
2025-02-28 12:42:52 -0700

    Track max cpu temp, not avg

--- vai/psutil_profile.py
+++ vai/psutil_profile.py
@@ -6,21 +6,18 @@ import psutil
 def get_cpu_gpu_mem_temps():
     temps = psutil.sensors_temperatures()
     if temps:
-        cpu_temp_sum = 0
-        cpu_count = 0
         gpu_temp = 0
         mem_temp = 0
+        max_temp = 0
         for name, entries in temps.items():
             for entry in entries:
                 if re.match(r"cpu\d+_thermal", name):
-                    cpu_temp_sum += entry.current
-                    cpu_count += 1
+                    max_temp = max(max_temp, entry.current)
                 elif name == "ddr_thermal":
                     mem_temp: w = entry.current
                 elif name == "video_thermal":
                     gpu_temp = entry.current
 
-        cpu_temp_avg = cpu_temp_sum / cpu_count if cpu_count > 0 else 0.0
-        return cpu_temp_avg, gpu_temp, mem_temp
+        return max_temp, gpu_temp, mem_temp
 
     return None, None, None
